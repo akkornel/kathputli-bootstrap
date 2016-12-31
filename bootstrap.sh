56d35c1a246f3dc1b7a1ed2e3268cd9af242b321
A. Karl Kornel <karl@kornel.us>
2016-12-31 11:56:36 -0800

    First round of big additions
    STDOUT and STDERR now go to a file.
    We look up/dervice the instance ID, AZ, and region.
    We get the EFS ID from the NFS tag, update /etc/fstab, and mount it.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-echo "Hello!" > /tmp/bootstrap-complete
+# Send output to a log file
+exec 1>/tmp/bootstrap.log 2>&1
 
+# Install packages
+# * awscli is for lots of stuff
+# * jq is for processing awscli output
+# * nfs-common is to mount EFS
+DEBIAN_FRONTEND=noninteractive apt-get install -y awscli jq nfs-common
+
+INSTANCE_ID=$(curl -s http://169.254.169.254/2014-02-25/meta-data/instance-id)
+AWS_AZ=$(curl -s http://169.254.169.254/2014-02-25/meta-data/placement/availability-zone)
+AWS_REGION=$(echo $AWS_AZ | sed 's/[a-z]$//')
+echo "Identified instance ID $INSTANCE_ID, in AZ $AWS_AZ (region $AWS_REGION)"
+
+# Mount our bootstrap data EFS
+mkdir /mnt/efs
+EFS_ID=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --region $AWS_REGION | jq '.Reservations[0].Instances[0].Tags[] | select(.Key | contains("NFS")) | .Value' | sed 's/"//g')
+echo "EFS ID is $EFS_ID"
+echo "${EFS_ID}.efs.${AWS_REGION}.amazonaws.com:/ /mnt/efs nfs nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 1" >> /etc/fstab
+echo "Mounting EFS..."
+mount /mnt/efs
+
+# All done!
+touch /tmp/bootstrap-complete
 exit 0
